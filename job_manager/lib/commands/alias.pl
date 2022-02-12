3cb05cdd97c9505ea65c9dd5990ce89d9fbe7135
Tom Wilson <wilsonte@umich.edu>
2022-02-12 09:20:51 -0500

    alias in current shell

--- job_manager/lib/commands/alias.pl
+++ job_manager/lib/commands/alias.pl
@@ -21,49 +21,61 @@ sub mdiAlias {
 
     # parse the options and apply defaults
     my $alias = $options{'alias'} || "mdi";
-    my $profileFile = glob($options{'profile'} || "~/.bashrc");
-    my $outLine = "alias $alias=\"$ENV{MDI_DIR}/mdi\" # written by MDI alias\n";
+    my $bashrc = "~/.bashrc";
+    my $profileFile = glob($options{'profile'} || $bashrc);
+    my $aliasCommand = "alias $alias=\"$ENV{MDI_DIR}/mdi\"";
+    my $outLine = "$aliasCommand # written by MDI alias\n";
 
-    # check the profile file path
-    -f $profileFile or throwError("file not found:\n    $profileFile", 'alias');
+    unless($options{'temporary'}){
 
-    # get user permission to modify their profile
-    getPermissionGeneral(
-        "The following line:\n".
-        "    $outLine".  
-        "will be written to file:\n".
-        "    $profileFile\n"
-    );
+        # get user permission to modify their profile
+        getPermissionGeneral(
+            "The following line:\n".
+            "    $outLine".  
+            "will be written to file:\n".
+            "    $profileFile\n"
+        );
 
-    # collect the contents of the current file as an array of lines
-    my ($replaced, @profile);    
-    open my $inH, "<", $profileFile or die "could not read file: $profileFile: $!\n";
-    while (my $line = <$inH>){
-        $line =~ m/\n/ or $line = "$line\n"; # guard against incomplete final line
-        $line eq $outLine and exit; # nothing to do, exit quietly
-        if($line =~ m/^alias\s+$alias=/){ 
-            getPermissionGeneral(
-                "Alias '$alias' already exists and will be overwritten from:\n". 
-                "    $line".
-                "to:\n".
-                "    $outLine"
-            );
-            push @profile, $outLine; 
-            $replaced = 1;
-        } else {
-            push @profile, $line;
+        # check the profile file path
+        if($profileFile eq $bashrc and !-f $profileFile){
+            open HANDLE, ">>$profileFile" or die "touch failed: $profileFile: $!\n";
+            close HANDLE;
+        } 
+        -f $profileFile or throwError("file not found:\n    $profileFile", 'alias');
+
+        # collect the contents of the current file as an array of lines
+        my ($replaced, @profile);    
+        open my $inH, "<", $profileFile or die "could not read file: $profileFile: $!\n";
+        while (my $line = <$inH>){
+            $line =~ m/\n/ or $line = "$line\n"; # guard against incomplete final line
+            $line eq $outLine and exit; # nothing to do, exit quietly
+            if($line =~ m/^alias\s+$alias=/){ 
+                getPermissionGeneral(
+                    "Alias '$alias' already exists and will be overwritten from:\n". 
+                    "    $line".
+                    "to:\n".
+                    "    $outLine"
+                );
+                push @profile, $outLine; 
+                $replaced = 1;
+            } else {
+                push @profile, $line;
+            }
         }
-    }
-    close $inH;
-    $replaced or push @profile, $outLine;  
+        close $inH;
+        $replaced or push @profile, $outLine;  
 
-    # print the new file
-    my $buFile = "$profileFile.mdiAliasBackup";
-    -e $buFile or copy($profileFile, $buFile);
-    open my $outH, ">", $profileFile or die "could not write file: $profileFile: $!\n";
-    print $outH join("", @profile);
-    close $outH;
-    exit;
+        # print the new file
+        my $buFile = "$profileFile.mdiAliasBackup";
+        -e $buFile or copy($profileFile, $buFile);
+        open my $outH, ">", $profileFile or die "could not write file: $profileFile: $!\n";
+        print $outH join("", @profile);
+        close $outH;
+
+    }
+
+    # finally, always add the alias to the user's current shell
+    exec $aliasCommand;
 }
 #========================================================================
 
